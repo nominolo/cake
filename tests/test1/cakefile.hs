7feb9e965c421c564a3b015bc98a6f226317a988
Thomas Schilling <nominolo@googlemail.com>
2011-12-14 09:06:45 +0000

    Clean up first test.

--- tests/test1/cakefile.hs
+++ tests/test1/cakefile.hs
@@ -1,27 +1,6 @@
 import Development.Cake
-import Control.Monad.IO.Class ( liftIO )
-import Control.Concurrent ( threadDelay )
---import Control.Monad.Exception ( throwIO )
 
 main = cake $ do
   "AB.txt" *> \out -> do
-    liftIO $ threadDelay (10 * seconds)
     cat ["A.txt", "B.txt"] out
-
-  "CD.txt" *> \out ->
-     cat ["C.txt", "D.txt"] out
-
-  "ABCD.txt" *> \out ->
-     cat ["AB.txt", "CD.txt", "GH.txt"] out
-
-  "EF.txt" *> \out ->
-    fail "This rule is silly"
-
-  "GH.txt" *> \out -> do
-    cat ["A.txt", "AB.txt"] out
---    liftIO (readFile "mueanounaoe")
-    return ()
-
-  want ["ABCD.txt"]
-
-seconds = 1000000
+  want ["AB.txt"]
